02b8bcfea7ea79308e171e0dc8080d5b30d6eeff
Paria Shiri <paria.shiri+madev@markallengroup.com>
2026-06-12 16:58:04 +0100

    Add BookStore DbContext

---
+++ Data/BookStoreDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BookApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookApi.Data;
+
+public class BookStoreDbContext : DbContext
+{
+    public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Book> Books => Set<Book>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>()
+            .Property(x => x.Price)
+            .HasPrecision(18, 2);
+    }
+}
