eb3d97ce8de52fb40c3b2e64d969aecee03f69f3
Paria Shiri <paria.shiri+madev@markallengroup.com>
2026-06-12 16:24:28 +0100

    Add Book model

---
+++ Models/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace BookApi.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Author { get; set; } = string.Empty;
+
+    public string Isbn { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
+
+    public int StockQuantity { get; set; }
+
+    public DateTime PublishedDate { get; set; }
+}
