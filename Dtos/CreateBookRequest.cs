e3ec6ec3a5769257450bd0da0041bcf627908269
Paria Shiri <paria.shiri+madev@markallengroup.com>
2026-06-12 16:29:39 +0100

    Add CreateBookRequest DTO

---
+++ Dtos/CreateBookRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace BookApi.Dtos;
+
+public class CreateBookRequest
+{
+    public string Title { get; set; } = string.Empty;
+
+    public string Author { get; set; } = string.Empty;
+
+    public string Isbn { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
+
+    public int StockQuantity { get; set; }
+
+    public DateTime PublishedDate { get; set; }
+}
