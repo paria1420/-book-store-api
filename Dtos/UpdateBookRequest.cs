3a5608c27123070818cc82fea8efcf53106ac3a4
Paria Shiri <paria.shiri+madev@markallengroup.com>
2026-06-15 10:22:39 +0100

    Add validation to book requests

--- Dtos/UpdateBookRequest.cs
+++ Dtos/UpdateBookRequest.cs
@@ -1,15 +1,25 @@
-﻿namespace BookApi.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookApi.Dtos;
 
 public class UpdateBookRequest
 {
+    [Required]
+    [MaxLength(200)]
     public string Title { get; set; } = string.Empty;
 
+    [Required]
+    [MaxLength(150)]
     public string Author { get; set; } = string.Empty;
 
+    [Required]
+    [MaxLength(20)]
     public string Isbn { get; set; } = string.Empty;
 
+    [Range(0.01, 9999.99)]
     public decimal Price { get; set; }
 
+    [Range(0, 100000)]
     public int StockQuantity { get; set; }
 
     public DateTime PublishedDate { get; set; }
