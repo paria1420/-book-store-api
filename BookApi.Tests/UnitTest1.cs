321584f0aca945f0771adc6cec363c7d82349ef1
Paria Shiri <paria.shiri+madev@markallengroup.com>
2026-06-15 11:54:51 +0100

    Add books controller get tests

--- BookApi.Tests/UnitTest1.cs
+++ BookApi.Tests/UnitTest1.cs
@@ -1,15 +1,67 @@
-﻿namespace BookApi.Tests;
+﻿using BookApi.Controllers;
+using BookApi.Dtos;
+using BookApi.Services;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+namespace BookApi.Tests;
 
-public class Tests
+[TestFixture]
+public class BooksControllerTests
 {
+    private Mock<IBookService> _bookServiceMock = null!;
+    private BooksController _controller = null!;
+
     [SetUp]
-    public void Setup()
+    public void SetUp()
     {
+        _bookServiceMock = new Mock<IBookService>();
+        _controller = new BooksController(_bookServiceMock.Object);
     }
 
     [Test]
-    public void Test1()
+    public async Task GetBooks_WhenBooksExist_ReturnsOkWithPagedResult()
     {
-        Assert.Pass();
+        // Arrange
+        var request = new GetBooksRequest
+        {
+            PageNumber = 1,
+            PageSize = 10
+        };
+
+        var pagedResult = new PagedResult<BookResponse>
+        {
+            Items =
+            [
+                new BookResponse
+                {
+                    Id = 1,
+                    Title = "Clean Code",
+                    Author = "Robert C. Martin",
+                    Isbn = "9780132350884",
+                    Price = 29.99m,
+                    StockQuantity = 5,
+                    PublishedDate = new DateTime(2008, 8, 1)
+                }
+            ],
+            PageNumber = 1,
+            PageSize = 10,
+            TotalCount = 1
+        };
+
+        _bookServiceMock
+            .Setup(x => x.GetAllAsync(It.IsAny<GetBooksRequest>()))
+            .ReturnsAsync(pagedResult);
+
+        // Act
+        var result = await _controller.GetBooks(request);
+
+        // Assert
+        var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+        var response = okResult.Value.Should().BeOfType<PagedResult<BookResponse>>().Subject;
+
+        response.Items.Should().HaveCount(1);
+        response.Items[0].Title.Should().Be("Clean Code");
+        response.TotalCount.Should().Be(1);
     }
 }
